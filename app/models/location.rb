c30579591cadcd328ffba4a97f0f953334c06485
cristimiro <cristian.miron.97@gmail.com>
2025-03-01 14:51:43 +0200

    Add location attributes validation

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,9 @@ class Location
 
   attr_accessor :lat, :long
 
+  validates :lat, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :long, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+
   def initialize(lat:, long:)
     @lat = lat
     @long = long
