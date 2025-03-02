161162856b7fa4bbd25f2d1548bf4380ea8d8bf6
cristimiro <cristian.miron.97@gmail.com>
2025-03-02 18:33:25 +0200

    Add CsvParser implementation

--- app/interactors/csv_parser.rb
+++ app/interactors/csv_parser.rb
@@ -4,5 +4,11 @@ class CsvParser
   include Interactor
 
   def call
+    begin
+      parsed_data = CSV.parse(context.csv_content, headers: context.headers)
+      context.csv_line_items = parsed_data.map(&:to_h)
+    rescue StandardError => e
+      context.fail!(error: "CSV parsing failed: #{e.message}")
+    end
   end
 end
