e25ac64b523bcaf6542585627b5ebfe50a51b5f8
cristimiro <cristian.miron.97@gmail.com>
2025-03-20 20:13:15 +0200

    Add CoffeeShop model validation specs

--- spec/models/coffee_shop_spec.rb
+++ spec/models/coffee_shop_spec.rb
@@ -1,35 +1,55 @@
 require "rails_helper"
 
 RSpec.describe CoffeeShop, type: :model do
-  subject { described_class.new(name: name, location: location) }
+  subject { described_class.new(params) }
 
-  # Deprecated
-  xcontext "Validations" do
-    context "when name and location present" do
-      let(:name) { "CoffeeName" }
-      let(:location) { Location.new(lat: 45.0, long: 45.0) }
+  let(:params) do
+    {
+      name: "Cristian",
+      lat: lat,
+      long: long,
+      address: "Turnu Rosu",
+      start_time: start_time,
+      end_time: end_time
+    }
+  end
+  let(:lat) { 45.0 }
+  let(:long) { 45.0 }
+  let(:start_time) { "09:00" }
+  let(:end_time) { "21:00" }
 
-      it "is valid" do
-        expect(subject).to be_valid
-      end
+  describe "#valid?" do
+    it { is_expected.to be_valid }
+
+    context 'when invalid lat' do
+      let(:lat) { 200.0 }
+
+      it { is_expected.not_to be_valid }
     end
 
-    context "when name is not present" do
-      let(:name) { nil }
-      let(:location) { Location.new(lat: 45.0, long: 45.0) }
+    context 'when invalid long' do
+      let(:long) { 200.0 }
 
-      it "is valid" do
-        expect(subject).to_not be_valid
-      end
+      it { is_expected.not_to be_valid }
     end
 
-    context "when location is not present" do
-      let(:name) { "CoffeeName" }
-      let(:location) { nil }
+    context 'when invalid start time' do
+      let(:start_time) { "-09:00" }
 
-      it "is valid" do
-        expect(subject).to_not be_valid
-      end
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'when invalid end time' do
+      let(:start_time) { "33:00" }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'when invalid interval' do
+      let(:start_time) { "21:00" }
+      let(:end_time) { "09:00" }
+
+      it { is_expected.not_to be_valid }
     end
   end
 end
