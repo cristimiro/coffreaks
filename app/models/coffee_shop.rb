51413cbae4abfaa8cafcac35fecb0bffb8e96381
cristimiro <cristian.miron.97@gmail.com>
2025-03-20 19:55:40 +0200

    Update CoffeeShop model to extend ApplicationRecord

--- app/models/coffee_shop.rb
+++ app/models/coffee_shop.rb
@@ -1,13 +1,2 @@
-class CoffeeShop
-  include ActiveModel::Model
-
-  attr_accessor :name, :location
-
-  validates :name, presence: true
-  validates :location, presence: true
-
-  def initialize(name:, location:)
-    @name = name
-    @location = location
-  end
+class CoffeeShop < ApplicationRecord
 end
