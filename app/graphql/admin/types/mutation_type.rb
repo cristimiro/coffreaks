cdefe1b44aaca76c23a579f429cb442f981af0df
cristimiro <cristian.miron.97@gmail.com>
2025-03-20 21:56:45 +0200

    Add UpdateCoffeeShop mutation definition and implementation

--- app/graphql/admin/types/mutation_type.rb
+++ app/graphql/admin/types/mutation_type.rb
@@ -4,6 +4,7 @@ module Admin
   module Types
     class MutationType < ::Types::BaseObject
       field :create_coffee_shop, mutation: Mutations::CreateCoffeeShop
+      field :update_coffee_shop, mutation: Mutations::UpdateCoffeeShop
       field :delete_coffee_shop, mutation: Mutations::DeleteCoffeeShop
     end
   end
