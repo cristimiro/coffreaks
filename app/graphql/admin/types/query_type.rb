357b94105c020d1f15f416630954c16b07212d1a
cristimiro <cristian.miron.97@gmail.com>
2025-03-20 09:52:56 +0200

    Define query type coffee_shops field

--- app/graphql/admin/types/query_type.rb
+++ app/graphql/admin/types/query_type.rb
@@ -3,6 +3,23 @@
 module Admin
   module Types
     class QueryType < ::Types::BaseObject
+      field :coffee_shops, [CoffeeShopType], null: false do
+        argument :name, String, required: false
+      end
+
+      def coffee_shops(name: "")
+        [
+          {
+            id: 1,
+            name: "CoffeShop Sibiu",
+            lat: 90.0,
+            long: 90.0,
+            address: "Calea poplacii",
+            start_time: "09:00",
+            end_time: "21:00"
+          }
+        ]
+      end
     end
   end
 end
